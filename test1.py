d449e3d76c82b4a7f52d390b66cb56afb7f1f74a
Maxime Laplace <maxime.laplace29@gmail.com>
2022-11-02 23:17:38 +0100

    enable more tests

--- test1.py
+++ test1.py
@@ -1,23 +1,10 @@
-#!/usr/bin/env python3
-# NeoPixel library strandtest example
-# Author: Tony DiCola (tony@tonydicola.com)
-#
-# Direct port of the Arduino NeoPixel library strandtest example.  Showcases
-# various animations on a strip of NeoPixels.
-
-import time
-from rpi_ws281x import PixelStrip, Color
 import argparse
+import time
 
-# LED strip configuration:
-LED_COUNT = 149        # Number of LED pixels.
-LED_PIN = 18          # GPIO pin connected to the pixels (18 uses PWM!).
-# LED_PIN = 10        # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
-LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
-LED_DMA = 10          # DMA channel to use for generating signal (try 10)
-LED_BRIGHTNESS = 255  # Set to 0 for darkest and 255 for brightest
-LED_INVERT = False    # True to invert the signal (when using NPN transistor level shift)
-LED_CHANNEL = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+from rpi_ws281x import Color, PixelStrip
+
+from config import (LED_BRIGHTNESS, LED_CHANNEL, LED_COUNT, LED_DMA,
+                    LED_FREQ_HZ, LED_INVERT, LED_PIN)
 
 
 # Define functions which animate LEDs in various ways.
@@ -27,7 +14,7 @@ def colorWipe(strip, color, wait_ms=50):
         strip.setPixelColor(i, color)
         strip.show()
         time.sleep(wait_ms / 1000.0)
-        
+
 def colorSegment(strip, color, startIndex, endIndex):
     for i in range(startIndex, endIndex):
         strip.setPixelColor(i, color)
@@ -109,9 +96,8 @@ if __name__ == '__main__':
 
         while True:
             print('Ouais Ouais Ouais')
-            colorWipe(strip, Color(255, 0, 0), 400)
+            colorWipe(strip, Color(255, 0, 0))
             time.sleep(1)
-            '''
             print('Color wipe animations.')
             colorWipe(strip, Color(255, 0, 0))  # Red wipe
             colorWipe(strip, Color(0, 255, 0))  # Green wipe
@@ -123,7 +109,7 @@ if __name__ == '__main__':
             print('Rainbow animations.')
             rainbow(strip)
             rainbowCycle(strip)
-            theaterChaseRainbow(strip)'''
+            theaterChaseRainbow(strip)
 
     except KeyboardInterrupt:
         if args.clear:
