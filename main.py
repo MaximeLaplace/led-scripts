88bc2a379284345b3d25472a369af547612ad40a
Maxime Laplace <maxime.laplace29@gmail.com>
2022-11-05 16:57:36 +0100

    create main module

---
+++ main.py
@@ -0,0 +1,44 @@
+import argparse
+
+from rpi_ws281x import PixelStrip
+
+from config import (
+    LED_BRIGHTNESS,
+    LED_CHANNEL,
+    LED_COUNT,
+    LED_DMA,
+    LED_FREQ_HZ,
+    LED_INVERT,
+    LED_PIN,
+)
+from src.scripts.menu_selection import menu
+from src.strip_utils.reset_strip import reset_strip
+
+if __name__ == "__main__":
+    # Process arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-c", "--clear", action="store_true", help="clear the display on exit"
+    )
+    args = parser.parse_args()
+
+    mode, args = menu()
+
+    strip = PixelStrip(
+        LED_COUNT,
+        LED_PIN,
+        LED_FREQ_HZ,
+        LED_DMA,
+        LED_INVERT,
+        LED_BRIGHTNESS,
+        LED_CHANNEL,
+    )
+    strip.begin()
+
+    try:
+        while True:
+            mode(strip, *args)
+
+    except KeyboardInterrupt:
+        if args.clear:
+            reset_strip(strip)
