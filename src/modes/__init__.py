1067312b18858e775b63a0325c9bccd7facafcf9
Maxime Laplace <maxime.laplace29@gmail.com>
2022-11-05 19:21:51 +0100

    add rainbow mode

--- src/modes/__init__.py
+++ src/modes/__init__.py
@@ -1,3 +1,4 @@
 from .color_wipe import color_wipe
+from .rainbow import rainbow
 from .strobo import strobo
 from .theater_chase import theater_chase
---
+++ src/modes/rainbow.py
@@ -0,0 +1,28 @@
+import time
+
+from rpi_ws281x import Color
+
+
+def wheel(pos):
+    """Generate rainbow colors across 0-255 positions."""
+    if pos < 85:
+        return Color(pos * 3, 255 - pos * 3, 0)
+    elif pos < 170:
+        pos -= 85
+        return Color(255 - pos * 3, 0, pos * 3)
+    else:
+        pos -= 170
+        return Color(0, pos * 3, 255 - pos * 3)
+
+
+def _rainbow_controller(strip, wait_ms=20, iterations=1):
+    """Draw rainbow that fades across all pixels at once."""
+    for j in range(256 * iterations):
+        for i in range(strip.numPixels()):
+            strip.setPixelColor(i, wheel((i + j) & 255))
+        strip.show()
+        time.sleep(wait_ms / 1000.0)
+
+
+def rainbow(strip, wait_ms: int = 20, iterations: int = 1):
+    _rainbow_controller(strip, wait_ms=wait_ms, iterations=iterations)
