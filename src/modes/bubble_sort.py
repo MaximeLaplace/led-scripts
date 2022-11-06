cc63f4296da728eaa538caa3bfb93e2ac3c8958b
Maxime Laplace <maxime.laplace29@gmail.com>
2022-11-06 20:59:03 +0100

    tweak bubble sort mode

--- src/modes/bubble_sort.py
+++ src/modes/bubble_sort.py
@@ -7,8 +7,8 @@ from .utils.color_wheel import create_rainbow_array
 
 def bubble_sort(strip, wait_ms: int = 100, infinite: bool = False):
     rainbow_array = create_rainbow_array(strip)
-    random.shuffle(rainbow_array)
-    rainbow_array.sort()
+    # random.shuffle(rainbow_array)
+    # rainbow_array.sort()
 
     address_led_with_array(strip, rainbow_array)
     strip.show()
