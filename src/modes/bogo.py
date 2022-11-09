dd9cedb3c482741108d60009d178b47ab85ecad7
Benjamin Poux <benjamin.pouxbouret@student-cs.fr>
2022-11-10 00:34:54 +0100

    fix: unused variable

--- src/modes/bogo.py
+++ src/modes/bogo.py
@@ -8,14 +8,13 @@ from .utils.rainbow import create_rainbow_array
 def _bogo_controller(
     strip,
     wait_ms: int = 100,
-    chance_of_light: float = 1.0,
     duration_s: int = 10,
     infinite: bool = False,
 ):
     time_left = init_time(duration_s)
     rainbow_array = create_rainbow_array(strip)
 
-    while time_left() > 0 or infinite:
+    while infinite or time_left() > 0:
         random.shuffle(rainbow_array)
         strip.setArrayColor(rainbow_array)
         strip.show()
@@ -25,7 +24,6 @@ def _bogo_controller(
 def bogo(
     strip,
     wait_ms: int = 100,
-    chance_of_light: float = 0.5,
     durations_s: int = 10,
     infinite: bool = False,
 ):
@@ -34,12 +32,10 @@ def bogo(
     Args:
         wait_ms (int): temps d'attente entre chaque mélange en ms
         durations_s (int): temps total d'éxécution
-        chance_of_lighting (int): proba qu'une LED soit allumée (entre 0 et 1)
     """
     _bogo_controller(
         strip,
         wait_ms=wait_ms,
-        chance_of_light=chance_of_light,
         duration_s=durations_s,
         infinite=infinite,
     )
