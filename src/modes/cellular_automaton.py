c149359e6106b85bb0586df0bc6c282838df5ac6
Benjamin Poux <benjamin.pouxbouret@student-cs.fr>
2022-11-10 00:49:48 +0100

    feat: added utils.circular_list

--- src/modes/cellular_automaton.py
+++ src/modes/cellular_automaton.py
@@ -3,24 +3,7 @@ import time
 from rpi_ws281x import Color
 
 from .utils.init_time import init_time
-
-
-class CircularList(list):
-    """List but you can have index and slices out of range"""
-    def __getitem__(self, key):
-        if isinstance(key, slice ) :
-            # Get the start, stop, and step from the slice without ceiling the stop
-            start, stop, step = key.indices(10**6)
-            if stop < start and step > 0: # From left to right
-                return [self[i] for i in range(start,stop+len(self),step)]
-            elif stop > start and step < 0: # From right to left
-                return [self[i] for i in range(start+len(self),stop,step)]
-            else:
-                return [self[i] for i in range(start,stop,step)]
-        elif isinstance( key, int ) :
-            return super(CircularList, self).__getitem__(key % len(self))
-        else:
-            raise TypeError("Invalid argument type.")
+from .utils.circular_list import CircularList
 
 
 def tertiary_decomposition(x):
