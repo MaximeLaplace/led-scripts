bed3dab25fa42120fdd4b9a9e2949de680ee12cb
Maxime Laplace <maxime.laplace29@gmail.com>
2022-11-09 15:22:38 +0100

    refactor strobo

--- src/modes/strobo.py
+++ src/modes/strobo.py
@@ -1,59 +1,17 @@
 import time
 
 from rpi_ws281x import Color
-from src.modes.utils.reset_strip import reset_strip
 
 from .utils.init_time import init_time
 
 
-def _strobo_controller(
+def strobo(
     strip,
     color=Color(255, 255, 255),
     wait_ms=50,
     duration_s: int = 10,
     infinite: bool = False,
 ):
-    """stroboscope lights"""
-    time_left = init_time(duration_s)
-
-    while time_left() > 0 or infinite:
-
-        for led in range(13, 50, 2):
-            strip.setPixelColor(led, color)
-        strip.show()
-        time.sleep(wait_ms / 1000.0)
-        reset_strip(strip)
-        time.sleep(wait_ms / 1000.0)
-
-        for led in range(78, 115, 2):
-            strip.setPixelColor(led, color)
-        strip.show()
-        time.sleep(wait_ms / 1000.0)
-        reset_strip(strip)
-        time.sleep(wait_ms / 1000.0)
-
-        for led in range(14, 50, 2):
-            strip.setPixelColor(led, color)
-        strip.show()
-        time.sleep(wait_ms / 1000.0)
-        reset_strip(strip)
-        time.sleep(wait_ms / 1000.0)
-
-        for led in range(79, 115, 2):
-            strip.setPixelColor(led, color)
-        strip.show()
-        time.sleep(wait_ms / 1000.0)
-        reset_strip(strip)
-        time.sleep(wait_ms / 1000.0)
-
-
-def strobo(
-    strip,
-    color: tuple[int, int, int] = (255, 255, 255),
-    wait_ms: int = 50,
-    duration_s: int = 10,
-    infinite: bool = False,
-) -> None:
     """Crée un strobo
 
     Args:
@@ -61,6 +19,25 @@ def strobo(
         wait_ms (int): Le temps d'attente entre chaque flash en millisecondes
         duration_s (int): Combien de temps au total en secondes
     """
-    _strobo_controller(
-        strip, Color(*color), wait_ms=wait_ms, duration_s=duration_s, infinite=infinite
-    )
+    time_left = init_time(duration_s)
+
+    offset = 0
+    segments = (1, 5)
+
+    while time_left() > 0 or infinite:
+        for segment in segments:
+            (strip.segments[s].setColor(0) for s in segments)
+
+            for i in range(offset, strip.segments[segment].numPixels(), 2):
+                strip.segments[segment].setPixelColor(i, color)
+
+            strip.show()
+            time.sleep(wait_ms / 1000)
+
+            strip.segments[segment].setColor(0)
+            strip.show()
+
+            time.sleep(wait_ms / 1000)
+        offset = -1 * offset + 1
+
+    return
