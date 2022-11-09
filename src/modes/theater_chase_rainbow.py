9e0b7fe2c5cbf3e334286eddeaa147f54d819c27
Maxime Laplace <maxime.laplace29@gmail.com>
2022-11-09 14:46:33 +0100

    refactor theater chase rainbow

--- src/modes/theater_chase_rainbow.py
+++ src/modes/theater_chase_rainbow.py
@@ -1,40 +1,27 @@
 import time
 
-from .utils.color_wheel import color_wheel
+from .utils.address_led_with_array import address_led_with_array
+from .utils.color_wheel import color_wheel, create_rainbow_array
 from .utils.init_time import init_time
-
-
-def _theater_chase_rainbow_controller(
-    strip,
-    wait_ms=50,
-    duration_s: int = 10,
-    infinite: bool = False,
-):
-    """Rainbow movie theater light style chaser animation."""
-    time_left = init_time(duration_s)
-
-    while time_left() > 0 or infinite:
-        for j in range(256):
-            for q in range(3):
-                for i in range(0, strip.numPixels(), 3):
-                    strip.setPixelColor(i + q, color_wheel((i + j) % 255))
-                strip.show()
-                time.sleep(wait_ms / 1000.0)
-                for i in range(0, strip.numPixels(), 3):
-                    strip.setPixelColor(i + q, 0)
+from .utils.reset_strip import reset_strip
 
 
 def theater_chase_rainbow(
-    strip,
-    wait_ms: int = 50,
-    duration_s: int = 10,
-    infinite: bool = False,
+    strip, wait_ms: int = 50, duration_s: int = 10, infinite: bool = False
 ):
     """Des petites lights qui tournent en étant en arc-en-ciel
 
     Args:
         wait_ms (int): Le temps d'attente entre chaque step en ms (petit = rapide)
     """
-    _theater_chase_rainbow_controller(
-        strip, wait_ms=wait_ms, duration_s=duration_s, infinite=infinite
-    )
+    time_left = init_time(duration_s)
+    rainbow_array = create_rainbow_array(strip)
+
+    while time_left() > 0 or infinite:
+        for offset in range(3):
+            reset_strip(strip)
+            for i in range(offset, strip.numPixels(), 3):
+                strip.setPixelColor(i, rainbow_array[i])
+            strip.show()
+            rainbow_array.append(rainbow_array.pop(0))
+            time.sleep(wait_ms / 1000)
