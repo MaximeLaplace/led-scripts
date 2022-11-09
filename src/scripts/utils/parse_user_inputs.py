f27c70e63c16dc9c5e51a6825b2d7d618c65eeb0
Maxime Laplace <maxime.laplace29@gmail.com>
2022-11-09 13:33:06 +0100

    refactor parse user inputs

--- src/scripts/utils/parse_user_inputs.py
+++ src/scripts/utils/parse_user_inputs.py
@@ -3,6 +3,21 @@ from typing import Callable, Union
 from .create_inquirers import get_function_parameters, get_tuple_types
 
 
+def parse_input(_input, _type):
+    if _type == int:
+        return int(_input)
+    elif _type == float:
+        return float(_input.replace(",", "."))
+    elif _type() == tuple():
+        tuple_types = get_tuple_types(_type)
+        return tuple(
+            [
+                int(v) if t == "int" else v
+                for v, t in zip(_input.split(","), tuple_types)
+            ]
+        )
+
+
 def parse_user_inputs(
     function: Callable, user_inputs: dict[str, str]
 ) -> dict[str, Union[str, int, tuple]]:
@@ -13,17 +28,6 @@ def parse_user_inputs(
     for user_input, (param_name, param_type) in zip(
         user_inputs.values(), function_parameters
     ):
-        if param_type == int:
-            parsed_inputs[param_name] = int(user_input)
-        elif param_type == float:
-            parsed_inputs[param_name] = float(user_input.replace(",", "."))
-        elif param_type() == tuple():
-            tuple_types = get_tuple_types(param_type)
-            parsed_inputs[param_name] = tuple(
-                [
-                    int(v) if t == "int" else v
-                    for v, t in zip(user_input.split(","), tuple_types)
-                ]
-            )
+        parsed_inputs[param_name] = parse_input(user_input, param_type)
 
     return parsed_inputs
