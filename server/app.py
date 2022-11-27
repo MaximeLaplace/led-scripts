694f880552419e36c8320f6ab86a8b5bdd898cb9
Maxime Laplace <maxime.laplace29@gmail.com>
2022-11-27 23:43:58 +0100

    update back create front

--- server/app.py
+++ server/app.py
@@ -1,11 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(
+    __name__,
+    static_folder="../front/build",
+    static_url_path="",
+    template_folder="../front/build",
+)
 
 
 @app.route("/")
 def home():
     return "Hello world !"
+
+
+@app.route("/react")
+def react():
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
