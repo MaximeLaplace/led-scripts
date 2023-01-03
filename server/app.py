9e7964a959a85f55b56ff5cc2ba039b3c8ac31d6
Maxime Laplace <maxime.laplace@ovrsea.com>
2023-01-03 23:04:15 +0100

    move telegram message to app definition

--- server/app.py
+++ server/app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template
 from flask_cors import CORS
-
+import socket
 import server.src.globals as globals_
+from telegrambot.send_message import send_ip
 
 from .src.routers import segments_to_use_bp, speed_factor_bp, start_mode_bp
 
@@ -19,6 +20,13 @@ app.register_blueprint(segments_to_use_bp)
 app.register_blueprint(speed_factor_bp)
 
 globals_.init()
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(("8.8.8.8", 80))
+IP_ADDRESS = s.getsockname()[0]
+s.close()
+send_ip(IP_ADDRESS)
 
 
 @app.route("/")
--- webapp.py
+++ webapp.py
@@ -1,18 +1,7 @@
 import server.src.globals as globals_
 from server.app import app
-from telegrambot.send_message import send_ip
-import socket
 
 application = app
 
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.connect(("8.8.8.8", 80))
-IP_ADDRESS = s.getsockname()[0]
-s.close()
-
-
 if __name__ == "__main__":
-    print("sending ip")
-    send_ip(f"{IP_ADDRESS}:{5000}")
-    print("ip sent")
     application.run(debug=True, use_reloader=True)
